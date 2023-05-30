a3764f2fed0a71b9211a429cdfbefea690e902f5
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-05-30 19:10:04 +0200

    Starting to fight through the hurdle of learning Silk.NET

---
+++ RougelikeJumper/Program.cs
@@ -0,0 +1,55 @@
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+
+var app = new RougelikeJumper();
+app.Run();
+
+unsafe class RougelikeJumper
+{
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    private IWindow? window;
+
+    public void Run()
+    {
+        InitWindow();
+        InitVulkan();
+        MainLoop();
+        CleanUp();
+    }
+
+    private void InitWindow()
+    {
+        //Create a window.
+        var options = WindowOptions.DefaultVulkan with
+        {
+            Size = new Vector2D<int>(WIDTH, HEIGHT),
+            Title = "Vulkan"
+        };
+
+        window = Window.Create(options);
+        window.Initialize();
+
+        if (window.VkSurface is null)
+        {
+            throw new Exception("Windowing platform doesn't support Vulkan.");
+        }
+    }
+
+    private void InitVulkan()
+    {
+
+    }
+
+    private void MainLoop()
+    {
+        window!.Run();
+    }
+
+    private void CleanUp()
+    {
+        window?.Dispose();
+    }
+}
